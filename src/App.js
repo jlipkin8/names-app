31797f2e1ccf0e7ba42847f62fa5e25f849adcad
Josie Lipkin <jlipkin8@gmail.com>
2023-04-10 14:32:03 -0400

    Store names as state.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import UserInputForm from './UserInputForm';
+import NameList from './NameList';
 
 function App() {
+  const [names, setNames] = React.useState(['Josie', 'Jordan']);
   return (
     <div className="App">
       <header>Find A Name</header>
       <UserInputForm />
+      <NameList names={names} />
     </div>
   );
 }
