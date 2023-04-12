d46df4e87e718efe8ea7232193770ced13fbdf82
Josie Lipkin <jlipkin8@gmail.com>
2023-04-12 14:04:39 -0400

    Lift state up.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './App.css';
 import UserInputForm from './UserInputForm';
 import NameList from './NameList';
+import { NAMES } from './names';
 
 function App() {
-  const [names, setNames] = React.useState(['Josie', 'Jordan']);
+  const [names, setNames] = React.useState(NAMES);
+  const [letter, setLetter] = React.useState('');
+
   return (
     <div className="App">
-      <header>Find A Name</header>
-      <UserInputForm />
-      <NameList names={names} />
+      <header className="header">Find A Name</header>
+      <UserInputForm
+        className="sidecar"
+        letter={letter}
+        setLetter={setLetter}
+        names={names}
+        setNames={setNames}
+      />
+      <NameList className names={names} />
     </div>
   );
 }
