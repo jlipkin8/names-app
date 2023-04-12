8d50d58e1aba96709e4b3a8af65cc33614e72161
Josie Lipkin <jlipkin8@gmail.com>
2023-04-12 14:05:28 -0400

    Fake data.

---
+++ src/names.js
@@ -0,0 +1,99 @@
+export const NAMES = [
+  {
+    id: 1,
+    name: 'Ava',
+    syllables: 2,
+  },
+  {
+    id: 2,
+    name: 'Olivia',
+    syllables: 4,
+  },
+  {
+    id: 3,
+    name: 'Charlotte',
+    syllables: 2,
+  },
+  {
+    id: 4,
+    name: 'Isabella',
+    syllables: 4,
+  },
+  {
+    id: 5,
+    name: 'Amelia',
+    syllables: 4,
+  },
+  {
+    id: 6,
+    name: 'Sophia',
+    syllables: 3,
+  },
+  {
+    id: 7,
+    name: 'Emma',
+    syllables: 2,
+  },
+  {
+    id: 8,
+    name: 'Mia',
+    syllables: 2,
+  },
+  {
+    id: 9,
+    name: 'Evelyn',
+    syllables: 3,
+  },
+  {
+    id: 10,
+    name: 'Harper',
+    syllables: 2,
+  },
+  {
+    id: 11,
+    name: 'Luna',
+    syllables: 2,
+  },
+  {
+    id: 12,
+    name: 'Camila',
+    syllables: 3,
+  },
+  {
+    id: 13,
+    name: 'Gianna',
+    syllables: 3,
+  },
+  {
+    id: 14,
+    name: 'Elizabeth',
+    syllables: 4,
+  },
+  {
+    id: 15,
+    name: 'Eleanor',
+    syllables: 3,
+  },
+  {
+    id: 16,
+    name: 'Ella',
+    syllables: 2,
+  },
+  {
+    id: 17,
+    name: 'Abigail',
+    syllables: 3,
+  },
+
+  {
+    id: 18,
+    name: 'Avery',
+    syllables: 3,
+  },
+
+  {
+    id: 19,
+    name: 'Scarlett',
+    syllables: 2,
+  },
+];
