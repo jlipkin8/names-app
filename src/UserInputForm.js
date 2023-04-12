96be94f050b1a01811f4db4ad11884a358f10ebe
Josie Lipkin <jlipkin8@gmail.com>
2023-04-12 14:05:20 -0400

    Break component up.

--- src/UserInputForm.js
+++ src/UserInputForm.js
@@ -1,24 +1,67 @@
 import React from 'react';
+import styles from './UserInputForm.module.css';
 
-function UserInputForm() {
+function FirstLetterInput({ letter, setLetter }) {
+  function handleLetter(event) {
+    const newLetter = event.target.value;
+    console.log('newLetter', newLetter);
+    setLetter(newLetter);
+  }
   return (
-    <form>
+    <>
       <label htmlFor="first-letter-field">First Letter:</label>
-      <input id="first-letter-field" value="" />
+      <input
+        id="first-letter-field"
+        value={letter}
+        maxLength="1"
+        onChange={handleLetter}
+      />
       <span>Enter the first letter for the name</span>
-      <div>
-        <input
-          type="range"
-          id="syllable"
-          name="syllable-number"
-          min="1"
-          max="10"
-          value="1"
-          step="1"
-        />
-        <label htmlFor="syllable">Syllable Number</label>
-      </div>
-      <button>Find!</button>
+    </>
+  );
+}
+
+function SyllableSlider() {
+  const [numOfSyllables, setNumOfSyllables] = React.useState(1);
+  function handleSlider(event) {
+    setNumOfSyllables(event.target.value);
+  }
+  return (
+    <div>
+      <input
+        type="range"
+        id="syllable"
+        name="syllable-number"
+        min="1"
+        max="10"
+        value={numOfSyllables}
+        step="1"
+        onChange={handleSlider}
+      />
+      <label htmlFor="syllable">
+        Syllable Number: {numOfSyllables}
+      </label>
+    </div>
+  );
+}
+function UserInputForm({ letter, setLetter, names, setNames }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    // filter names here
+    const filteredNames = names.filter((name) => {
+      return name.name.startsWith(letter.toUpperCase());
+    });
+    console.log('filteredNames', filteredNames);
+    setNames(filteredNames);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <FirstLetterInput
+        letter={letter}
+        setLetter={setLetter}
+      ></FirstLetterInput>
+      <SyllableSlider></SyllableSlider>
+      <button className={styles.button}>Find!</button>
     </form>
   );
 }
