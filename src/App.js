5aba6c1c859b0317e9dc0e659d89aa590aa0ce42
Josie Lipkin <jlipkin8@gmail.com>
2023-04-13 15:45:57 -0400

    Lift more state up.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import { NAMES } from './names';
 
 function App() {
   const [names, setNames] = React.useState(NAMES);
+  const [displayedNames, setDisplayedNames] = React.useState([]);
   const [letter, setLetter] = React.useState('');
+  const [numOfSyllables, setNumOfSyllables] = React.useState(0);
 
   return (
     <div className="App">
       <header className="header">Find A Name</header>
       <UserInputForm
-        className="sidecar"
+        className="sidebar"
         letter={letter}
         setLetter={setLetter}
         names={names}
         setNames={setNames}
+        setDisplayedNames={setDisplayedNames}
+        numOfSyllables={numOfSyllables}
+        setNumOfSyllables={setNumOfSyllables}
       />
-      <NameList className names={names} />
+      <NameList className="content" names={displayedNames} />
     </div>
   );
 }
